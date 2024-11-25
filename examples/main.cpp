cb4c7647bcf19fcc1b90b62f43a0e7be0a5e1e84
Kenneth Assogba <kennethassogba@gmail.com>
2024-11-25 02:04:35 +0100

    Delete model.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -10,12 +10,11 @@ int main() {
   CheckOpenMP();
 
   ArrayXd sigma_t{{0.222222, 0.833333}};
-  ArrayXd D = 1. / (3. * sigma_t);
-  ArrayXd sigma_a{{0.010120, 0.080032}};
-  ArrayXd nu_sigma_f{{0., 0.135}};
-  ArrayXd sigma_f{{0., 0.135}};
-  ArrayXd chi{{1., 0.}};
   ArrayXXd sigma_s{{0.00, 0.02}, {0.00, 0.00}};
+  ArrayXd sigma_a{{0.010120, 0.080032}};
+  ArrayXd sigma_f{{0., 0.135}};
+  ArrayXd nu_sigma_f{{0., 0.135}};
+  ArrayXd chi{{1., 0.}};
 
   Material uo2(sigma_t, sigma_s, sigma_a, sigma_f, nu_sigma_f, chi, "UO2");
 
