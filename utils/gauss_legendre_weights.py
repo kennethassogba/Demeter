668b5299bde0b8f49efa09f9a783a9d97f27066d
Arthur <arthur.alb53@gmail.com>
2024-12-23 13:20:37 +0100

    Folder creation 4 useful py scripts

---
+++ utils/gauss_legendre_weights.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Resume: The script the roots and weights of the legendre polynomials
+# of order n. 
+# See https://mathworld.wolfram.com/Legendre-GaussQuadrature.html
+
+
+# w_i = (2 * (1-roots[i]**2)) / ((i+1)**2 * (PL(roots[i]))**2)
+
+
+# SUM(W_i) = 2.
